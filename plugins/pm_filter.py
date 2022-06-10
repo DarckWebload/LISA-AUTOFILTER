ff99110d4fa313d55d1e9b7c52e21a2b1f214c75
DarckWebload <106021000+DarckWebload@users.noreply.github.com>
2022-06-10 22:15:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -589,17 +589,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode='html'
         )
-    elif query.data == "ytthumb":
-        buttons = [[
-            InlineKeyboardButton('🚶‍♀️ ʙᴀᴄᴋ', callback_data='help')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
-            text=script.YTTHUMB_TXT,
-            disable_web_page_preview=True,
-            reply_markup=reply_markup,
-            parse_mode='html'
-        )
     elif query.data == "ttss":
         buttons = [[
             InlineKeyboardButton('🚶‍♀️ ʙᴀᴄᴋ', callback_data='help')
