3db1e449d9ed5e84cc9b2e12cd172b40709947b2
DarckWebload <106021000+DarckWebload@users.noreply.github.com>
2022-06-11 10:42:49 +0530

    Update url_shortner.py

--- plugins/Don/url_shortner.py
+++ plugins/Don/url_shortner.py
@@ -1,4 +1,4 @@
-	import os
+import os
 import aiohttp
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, InputTextMessageContent
